e7f60ba43a2ea12c17b7e2758dd9194cff6d58a9
melihacil <knghtmellomello@gmail.com>
2023-07-04 13:31:56 +0300

    Updating

--- SivasUniProject/Assets/Gamehandler.cs
+++ SivasUniProject/Assets/Gamehandler.cs
@@ -5,39 +5,11 @@ using UnityEngine.Video;
 
 public class Gamehandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     [SerializeField]
     VideoClip m_ArcheryClip;
-
-    [SerializeField]
-    VideoClip m_ArcheryClip;
-
-    [SerializeField]
-    VideoClip m_ArcheryClip;
-
 
     [SerializeField]
     List<VideoClip> m_videoClips = new List<VideoClip>();
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-
-
-    private string m_GameName = "okçuluk";
-
-    public void ChangeSelectedGame(string GameName)
-    {
-       this.m_GameName = GameName;
-    }
-
 
 }
