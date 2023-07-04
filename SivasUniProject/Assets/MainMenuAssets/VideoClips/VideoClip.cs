31124f83655646046965b53267aa2e1e8efda3e1
melihacil <knghtmellomello@gmail.com>
2023-07-04 14:00:53 +0300

    Update

---
+++ SivasUniProject/Assets/MainMenuAssets/VideoClips/VideoClip.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu]
+public class VideoClip : ScriptableObject
+{
+    [SerializeField]
+    VideoClip m_ArcheryClip;
+    [SerializeField]
+    string m_Name;
+}
