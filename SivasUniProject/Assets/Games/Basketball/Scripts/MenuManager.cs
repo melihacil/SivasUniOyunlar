b956617b25d5c7ba6c57ce8e5351711ea36563f5
melihacil <knghtmellomello@gmail.com>
2023-07-28 11:52:02 +0300

    Updating, removing and unifying redundant menu managers

--- SivasUniProject/Assets/Games/Basketball/Scripts/MenuManager.cs
+++ SivasUniProject/Assets/Games/Basketball/Scripts/MenuManager.cs
@@ -2,9 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+public enum MenuType
+{
+    ExitButton,
+    ResetButton
+}
 
 public class MenuManager : MonoBehaviour
 {
+
+   
+
+   public MenuType menuType = MenuType.ExitButton;
+
    void ExitGame()
    {
         Application.Quit();
