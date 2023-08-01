e38cfb11fefe021706f7a810bbf46f7e3067d9ed
melihacil <knghtmellomello@gmail.com>
2023-08-01 15:32:16 +0300

    Update

--- SivasUniProject/Assets/Games/FruitNinja/Scripts/SceneController.cs
+++ SivasUniProject/Assets/Games/FruitNinja/Scripts/SceneController.cs
@@ -18,6 +18,34 @@ public class SceneController : MonoBehaviour
 
     }
 
+    [SerializeField]
+    GameObject m_LightSaber;
+    [SerializeField]
+    GameObject m_Katana;
+
+    bool m_ChangeModelType = true;
+    [ContextMenu("Test")]
+    public void ChangeModel()
+    {
+
+        GameObject obj1 = leftHand.modelPrefab.gameObject;
+        GameObject obj2 = rightHand.modelPrefab.gameObject;
+
+        obj1.SetActive(false);
+        obj2.SetActive(false);
+        //if (m_ChangeModelType)
+        //{
+        //    m_ChangeModelType = !m_ChangeModelType;
+        //    leftHand.modelPrefab = m_LightSaber.transform;
+        //    rightHand.modelPrefab = m_LightSaber.transform;
+        //}
+        //else
+        //{
+        //    m_ChangeModelType = !m_ChangeModelType;
+        //    leftHand.modelPrefab = m_Katana.transform;
+        //    rightHand.modelPrefab = m_Katana.transform;
+        //}
+    }
 
 
 }
