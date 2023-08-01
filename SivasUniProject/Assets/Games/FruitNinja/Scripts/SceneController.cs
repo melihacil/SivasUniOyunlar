911ca457061e14d37aaf19e1e1553413e8523c72
melihacil <knghtmellomello@gmail.com>
2023-08-01 15:16:46 +0300

    Update SceneController.cs

--- SivasUniProject/Assets/Games/FruitNinja/Scripts/SceneController.cs
+++ SivasUniProject/Assets/Games/FruitNinja/Scripts/SceneController.cs
@@ -2,13 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.XR.Interaction.Toolkit;
 public class SceneController : MonoBehaviour
 {
     CanvasGroup canvasGroup;
+    [SerializeField]
+    XRController leftHand;
+    [SerializeField]
+    XRController rightHand;
+
 
     public void LoadThisScene(string _sceneName)
     {
         SceneManager.LoadScene(_sceneName);
 
     }
+
+
+
 }
