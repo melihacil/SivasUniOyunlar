aaebb09803c9ed5adb3434ae1ade73d23a88b71a
melihacil <knghtmellomello@gmail.com>
2023-08-02 13:40:54 +0300

    Updating golf game

--- SivasUniProject/Assets/Scripts/Concrete/Ozgur/VR GOLF/AddSpeedOnTouch.cs
+++ SivasUniProject/Assets/Scripts/Concrete/Ozgur/VR GOLF/AddSpeedOnTouch.cs
@@ -10,11 +10,17 @@ public class AddSpeedOnTouch : MonoBehaviour
     private Vector3 prevPos;
     private Vector3 velocity;
     private Collider clubCollider;
-    
+
     // Start is called before the first frame update
-    void Start()
+
+
+    private void Awake()
     {
         clubCollider = GetComponent<Collider>();
+    }
+    void Start()
+    {
+        
     }
 
     // Update is called once per frame
@@ -30,12 +36,17 @@ public class AddSpeedOnTouch : MonoBehaviour
         prevPos = transform.position;
     }
 
+
+    [SerializeField]
+    BoxCollider m_GolfClubRedundant;
+    [SerializeField]
+    private float m_ResetSpeed = 0.1f;
     private void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag(targetTag))
         {
             Debug.Log("Colliding...");
-
+            clubCollider.enabled = false;
             Vector3 collisionPos = clubCollider.ClosestPoint(other.transform.position);
             Vector3 collisionNorm = other.transform.position - collisionPos;
             Vector3 projectedVelocity = Vector3.Project(velocity, collisionNorm);
@@ -43,8 +54,17 @@ public class AddSpeedOnTouch : MonoBehaviour
 
             Rigidbody rb = other.attachedRigidbody;
             rb.velocity = projectedVelocity;
-
             gameManager.currentHitNumber++;
+
+            Invoke(nameof(OpenCollider), m_ResetSpeed);
         }
     }
+
+
+    private void OpenCollider()
+    {
+        clubCollider.enabled = true;
+    }
+
+
 }
