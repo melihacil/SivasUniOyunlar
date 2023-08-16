354c1a8260e4b25b782231e2664a97d20b85ac83
melihacil <knghtmellomello@gmail.com>
2023-08-16 11:32:35 +0300

    Update ObjectSpawner.cs
    Updating angles on spawner

--- SivasUniProject/Assets/Games/Archery/Scripts/ObjectSpawner.cs
+++ SivasUniProject/Assets/Games/Archery/Scripts/ObjectSpawner.cs
@@ -50,14 +50,47 @@ public class ObjectSpawner : MonoBehaviour
         }
     }
 
+
+    [SerializeField] float m_SpawnAngle;
     // From a circle to a cone shaped area
     private void SpawnObject()
     {
+
+        float angleInRad = Random.Range(0.0f, m_SpawnAngle) * Mathf.Deg2Rad;
         // Rastgele bir nokta seçme
-        Vector2 randomPoint = Random.insideUnitCircle.normalized;
+        Vector2 randomPoint = Random.insideUnitCircle.normalized * Mathf.Sin(angleInRad);
+        // This will give you a circle
         Vector3 spawnPosition = new Vector3(randomPoint.x, 0f, randomPoint.y) * Random.Range(innerRadius, outerRadius);
 
         // Nesneyi oluþturma
         Instantiate(RandomTarget(Random.Range(1,4)), centerPoint.position + spawnPosition, Quaternion.identity);
     }
+
+    private void SpawnObjectOld()
+    {
+
+        float angleInRad = Random.Range(0.0f, m_SpawnAngle) * Mathf.Deg2Rad;
+        // Rastgele bir nokta seçme
+        Vector2 randomPoint = Random.insideUnitCircle.normalized;
+        // This will give you a circle
+        Vector3 spawnPosition = new Vector3(randomPoint.x, 0f, randomPoint.y) * Random.Range(innerRadius, outerRadius);
+
+        // Nesneyi oluþturma
+        Instantiate(RandomTarget(Random.Range(1, 4)), centerPoint.position + spawnPosition, Quaternion.identity);
+    }
+
+    public static Vector3 GetPointOnUnitSphereCap(Quaternion targetDirection, float angle)
+    {
+        var angleInRad = Random.Range(0.0f, angle) * Mathf.Deg2Rad;
+        var PointOnCircle = (Random.insideUnitCircle.normalized) * Mathf.Sin(angleInRad);
+        var V = new Vector3(PointOnCircle.x, PointOnCircle.y, Mathf.Cos(angleInRad));
+        return targetDirection * V;
+    }
+
+    public static Vector3 GetPointOnUnitSphereCap(Vector3 targetDirection, float angle)
+    {
+        return GetPointOnUnitSphereCap(Quaternion.LookRotation(targetDirection), angle);
+    }
+
+
 }
