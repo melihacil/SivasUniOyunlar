e85b42abdd94e80257046e1b7fb1a84eb65a2c25
melihacil <knghtmellomello@gmail.com>
2023-09-17 02:10:06 +0300

    Updating bowling button scripts

--- SivasUniProject/Assets/Games/Bowling/Assets/Buton/VRButton.cs
+++ SivasUniProject/Assets/Games/Bowling/Assets/Buton/VRButton.cs
@@ -5,6 +5,7 @@ using UnityEngine.Events;
 
 public class VRButton : MonoBehaviour
 {
+    
     //Butona dokunulduktan sonra içeride biraz beklemsini saðlayan özellik
     public float deadTime = 1f;
     //Bool deðeri dead time süresince butonun kilitli kalmasýný saðlýyor.
@@ -13,28 +14,35 @@ public class VRButton : MonoBehaviour
     public UnityEvent onPressed, onReleased;
 
     public static bool on;
+
+
+
+    [SerializeField] private GameObject m_ButtonMesh;
+
+
+    private GameObject m_Hand = null;
     // Start is called before the first frame update
     void Start()
     {
         on = false;
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    [ContextMenu("TestTriggerEnter")]
     public void OnTriggerEnter(Collider other)
     {
         if(other.tag =="Button" && !_deadTimeActive)
         {
+
             onPressed?.Invoke();
             Debug.Log("Tuþa bastýn");
             on = true;
+            m_Hand = other.gameObject;
         }
+        m_ButtonMesh.transform.localPosition = new Vector3(0f,0.03f,0f);
+
+
     }
 
+    
    public void OnTriggerExit(Collider other)
     {
         if(other.tag=="Button" && !_deadTimeActive)
@@ -43,10 +51,26 @@ public class VRButton : MonoBehaviour
             Debug.Log("Tuþ serbest kaldý");
             StartCoroutine(WaitForDeadTime());
             
-            
+            //if (m_Hand =  other.gameObject)
+            //{
+
+            //}
         }
+       
     }
 
+
+    [ContextMenu("TestTriggerEnter")]
+    public void TestEnter()
+    {
+        m_ButtonMesh.transform.localPosition = new Vector3(0f, 0.033f, 0f);
+    }
+
+    [ContextMenu("TestTriggerExit")]
+    public void TestExit()
+    {
+        m_ButtonMesh.transform.localPosition = new Vector3(0f, 0.065f, 0f);
+    }
     IEnumerator WaitForDeadTime()
     {
         _deadTimeActive = true;
