fa6dbd6815051444d0f4b5628cf90acba36647ca
melihacil <knghtmellomello@gmail.com>
2023-09-16 21:27:54 +0300

    Cleaning bowling scripts

--- SivasUniProject/Assets/Games/Bowling/Assets/script/BowlingUIManager.cs
+++ SivasUniProject/Assets/Games/Bowling/Assets/script/BowlingUIManager.cs
@@ -7,17 +7,7 @@ public class BowlingUIManager : MonoBehaviour
 
     [SerializeField] Text ScoreText;
     private int score = 0;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     public void IncreaseScore()
     {
         ScoreText.text = "Score: " + score;
