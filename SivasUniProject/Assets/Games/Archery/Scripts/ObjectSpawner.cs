6beb40c46f14198b02d0b68aff2ce356e2f39351
melihacil <knghtmellomello@gmail.com>
2023-09-28 13:49:46 +0300

    Updating archery object spawner

--- SivasUniProject/Assets/Games/Archery/Scripts/ObjectSpawner.cs
+++ SivasUniProject/Assets/Games/Archery/Scripts/ObjectSpawner.cs
@@ -21,6 +21,11 @@ public class ObjectSpawner : MonoBehaviour
     private bool m_Spawnable = true;
 
 
+    [SerializeField] float m_SpawnAngleRedundant;
+    [SerializeField] Vector2 m_xAxis;
+    [SerializeField] Vector2 m_zAxis;
+
+    [SerializeField] private LayerMask m_ScoreLayer;
     private void Update()
     {
 
@@ -52,24 +57,35 @@ public class ObjectSpawner : MonoBehaviour
     }
 
 
-    [SerializeField] float m_SpawnAngleRedundant;
-    [SerializeField] Vector2 m_xAxis;
-    [SerializeField] Vector2 m_zAxis;
     // From a circle to a cone shaped area
     private void SpawnObject()
     {
 
 
-        // Rastgele bir nokta seçme
-        Vector2 randomPoint = Random.insideUnitCircle.normalized;
-        // This will give you a circle
-        //Vector3 spawnPosition = new Vector3(randomPoint.x, 0f, randomPoint.y) * Random.Range(innerRadius, outerRadius);
-
-        Vector3 clampedSpawnPosition = new Vector3(Random.Range(m_xAxis.x,m_xAxis.y), 0.2f, Random.Range(m_zAxis.x,m_zAxis.y));
-
+      
 
         // Nesneyi oluþturma
-        Instantiate(RandomTarget(Random.Range(1, 4)), clampedSpawnPosition, Quaternion.identity);
+
+        //LayerMask mask = LayerMask.GetMask("Default");
+
+        //mask.value = 0;
+        
+        for (;;) {
+            // Rastgele bir nokta seçme
+            Vector2 randomPoint = Random.insideUnitCircle.normalized;
+            // This will give you a circle
+            //Vector3 spawnPosition = new Vector3(randomPoint.x, 0f, randomPoint.y) * Random.Range(innerRadius, outerRadius);
+
+            Vector3 clampedSpawnPosition = new Vector3(Random.Range(m_xAxis.x, m_xAxis.y), 0.2f, Random.Range(m_zAxis.x, m_zAxis.y));
+
+            Collider[] results = Physics.OverlapSphere(clampedSpawnPosition, 2f, m_ScoreLayer.value);
+            if (results.Length == 0)
+            {
+                Instantiate(RandomTarget(Random.Range(1, 4)), clampedSpawnPosition, Quaternion.identity);
+                break;
+            }
+        }
+
     }
 
 
